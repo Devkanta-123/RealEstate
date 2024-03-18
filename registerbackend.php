abf70ea7cbb3db8cec7e95dad00ea50093263ce5
Your Name <you@example.com>
2024-03-18 22:36:52 +0530

    update db

--- registerbackend.php
+++ registerbackend.php
@@ -2,15 +2,6 @@
 // include db connection
 include 'db/db_connection.php';
 
-// Retrieve form data
-// var firstname = $('#firstname').val();
-// var lastname = $('#lastname').val();
-// var email = $('#email').val();
-// var phoneno = $('#phoneno').val();
-// var password = $('#password').val();
-
-
-
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $email = $_POST['email'];
